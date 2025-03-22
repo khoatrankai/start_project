d1d2c494ba15a563fb4039fbf79a53a6eb7e637f
Hieu <khoanono963@gmail.com>
2025-03-22 13:07:26 +0700

    up

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountUsers } from './entities/account_users.entity';
-import { RoleTypeUser } from './entities/role_type_user.entity';
-import { RoleUser } from './entities/role_user.entity';
-import { CategoryRoleUser } from './entities/category_role_user.entity';
 import { createConnection } from 'mysql2/promise';
 import { ConfigService } from '@nestjs/config';
-import { GroupUser } from './entities/group_user.entity';
-import { ListGroupRole } from './entities/list_group_role.entity';
-import { Notify } from './entities/notify.entity';
-import { NotifyRole } from './entities/notify_role.entity';
-import { NotifyUser } from './entities/notify_user.entity';
-import { TimeKeeping } from './entities/timekeeping.entity';
 
 @Module({
   imports: [
@@ -37,15 +28,6 @@ import { TimeKeeping } from './entities/timekeeping.entity';
           database: configService.get<string>('DB_NAME'),
           entities: [
             AccountUsers,
-            RoleTypeUser,
-            RoleUser,
-            CategoryRoleUser,
-            GroupUser,
-            ListGroupRole,
-            Notify,
-            NotifyRole,
-            NotifyUser,
-            TimeKeeping
           ],
           // synchronize: true,
           // dropSchema: true,
